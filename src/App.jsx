6c74e112a87a70e22a976b29c1d03d5368544a91
AlMamunTalukder <almamuntalukder0@gmail.com>
2023-09-15 22:39:07 +0600

    first commit where create and install course.JSON file. Now start the main coding

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Home from "./assets/Home/Home";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <Home></Home>
+    </>
+  );
+}
+
+export default App;
---
+++ src/assets/Home/Home.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import Cart from "../Cart/Cart";
+
+const Home = () => {
+  return (
+    <div className="main-container">
+      <div className="main-card">
+        <div className="card"></div>
+      </div>
+      <div className="cart">
+        <Cart></Cart>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
